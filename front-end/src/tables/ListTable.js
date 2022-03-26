c0cd7ead8c5eb49c7375d91df774cedab70b9ae5
Brandon Howle <64497871+BHowle@users.noreply.github.com>
2022-03-25 21:51:58 -0700

    Fixed key= issue

--- front-end/src/tables/ListTable.js
+++ front-end/src/tables/ListTable.js
@@ -14,8 +14,8 @@ export default function ListTable({ tables, finishHandler }) {
           </tr>
         </thead>
         <tbody>
-          {tables.map((table, index) => (
-            <tr key={index}>
+          {tables.map((table) => (
+            <tr key={table.table_id}>
               <th scope="row">{table.table_id}</th>
               <td>{table.table_name}</td>
               <td>{table.capacity}</td>
